75fe595cfc3f2c87072400c2e85b54b668586025
psst54 <psst5491@naver.com>
2024-09-24 14:56:31 +0900

    style: set text styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
 function App() {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <p className="title1">title1</p>
+      <p className="title2">title2</p>
+      <p className="title3">title3</p>
+      <p className="title4">title4</p>
+
+      <p className="body1">body1</p>
+      <p className="body2">body2</p>
+      <p className="body3">body3</p>
+      <p className="body4">body4</p>
+      <p className="body5">body5</p>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,40 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        black: '#000000',
+        dark_gray: '#6E6E6E',
+        white: '#FFFFFF',
+        blue: '#0070DA',
+        blue_gray: '#F2F5F7',
+        red: '#FE0135',
+      },
+      fontSize: {
+        title1: '22px',
+        title2: '16px',
+        title3: '16px',
+        title4: '16px',
+        body1: '14px',
+        body2: '14px',
+        body3: '14px',
+        body4: '12px',
+        body5: '12px',
+        button: '12px',
+      },
+      lineHeight: {
+        title1: '145%',
+        title2: '130%',
+        title3: '135%',
+        title4: '135%',
+        body1: '130%',
+        body2: '140%',
+        body3: '130%',
+        body4: '130%',
+        body5: '130%',
+        button: '135%',
+      },
+    },
   },
   plugins: [],
 };
