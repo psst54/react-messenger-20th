469cff496ca69e666a75b29fb9fb9b5b3edf12b5
psst54 <psst5491@naver.com>
2024-09-26 00:28:05 +0900

    style: change text styles

--- src/components/MessageList/MessageItem.tsx
+++ src/components/MessageList/MessageItem.tsx
@@ -1,6 +1,6 @@
 import { type Message } from 'src/hooks/useMessage';
 
-const defaultMessageText = 'body2 px-[12px] py-[8px] w-fit rounded-[18px]';
+const defaultMessageText = 'body2 px-[12px] py-[8px] w-fit max-w-[256px] rounded-[18px] break-keep';
 
 function MyMessage({
   message,
@@ -11,7 +11,7 @@ function MyMessage({
   isFirst: boolean;
   isLast: boolean;
 }) {
-  const myMessageStyle = `max-w-[256px] bg-blue text-white rounded-r-[4px] 
+  const myMessageStyle = `bg-blue text-white rounded-r-[4px] 
   ${isFirst ? 'rounded-tr-[18px]' : ''} 
   ${isLast ? 'rounded-br-[18px]' : ''}`;
 
